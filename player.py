fdf8d1f6fafddbef6a0bedd17a4360bef00c5930
Christopher Eugene Jackson <159967132+chrisJ1252@users.noreply.github.com>
2026-03-03 18:44:16 +0000

    implementing player, human can choose a column to add a disk in a 1-indexed way that maps to 0 indexed board

--- player.py
+++ player.py
@@ -11,14 +11,17 @@ class Player:
         # remember input is 1-indexed but your board is 0-indexed internally
         # TIP: if computer — just call ai.best_move(), no input needed
         if self.is_human == True: 
-            try: 
-                player_move = input("Choose a column to place disk: ")
-                while board_obj.checkValidMove(board, int(player_move)) == False:
-                    player_move = input("Column is full, please pick another column to add a disk")
 
+            human_move = input("Choose a column to place disk using a number displayed above: ")
+            human_move = int(human_move) - 1
+
+            try: 
+                while board_obj.checkValidMove(board, human_move) == False:
+                     human_move = input("Not a valid move, please pick another column to add a disk: ")
+
+                return human_move
             except: 
-                raise TypeError
-        return player_move
+                raise TypeError("You did not input a number")
 
             
 if __name__ == "__main__":
@@ -26,5 +29,7 @@ if __name__ == "__main__":
     board_obj = Board(5, 5, 2)
     board = board_obj.initializeBoard()
     board_obj.printBoard(board)
-    human_move = human.get_move(board, board_obj=board_obj)
-    board_obj.addDisk(board, human_move, human)
+    human_move = human.get_move(board, board_obj)
+    print(human_move)
+    board_obj.addDisk(board, int(human_move), human)
+    board_obj.printBoard(board)
